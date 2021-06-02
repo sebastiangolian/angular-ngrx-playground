2797c78c38d87f7ce39effc22c1d8b33408de17a
Sebastian Golian <sebastiangolian@gmail.com>
2021-06-02 10:15:49 +0200

    add ngrx: effects, entity, schematics

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './pages/app/app.component';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {})],
+  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {}), EffectsModule.forRoot([]), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
 })
