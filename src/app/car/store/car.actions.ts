9e1d600437932217af2ca566e29c689f2bf796f6
Sebastian Golian <sebastiangolian@gmail.com>
2021-07-11 15:57:31 +0200

    update

--- src/app/car/store/car.actions.ts
+++ src/app/car/store/car.actions.ts
@@ -3,8 +3,5 @@ import { Car } from '../pages/interfaces/car.interface';
 
 export const changeStatus = createAction('[Car Component] - change status');
 export const priceUp = createAction('[Car Component] - price up');
-export const setCar = createAction(
-  '[Car Component] - set car',
-  props<{ car: Car }>()
-);
+export const setCar = createAction('[Car Component] - set car', props<{ car: Car }>());
 export const resetCar = createAction('[Car Component] - reset');
--- src/app/car/store/car.reducer.ts
+++ src/app/car/store/car.reducer.ts
@@ -3,25 +3,14 @@ import { Car } from '../pages/interfaces/car.interface';
 import * as CarActions from './car.actions';
 
 export const key = 'car';
-export const initialState: Car = {
-  name: 'Seat',
-  price: 45000,
-  isEnabled: false,
-};
+export const initialState: Car = { name: 'Seat', price: 45000, isEnabled: false };
 
 const carReducer = createReducer(
   initialState,
   on(CarActions.priceUp, (state) => ({ ...state, price: state.price + 1 })),
-  on(CarActions.changeStatus, (state) => ({
-    ...state,
-    isEnabled: !state.isEnabled,
-  })),
+  on(CarActions.changeStatus, (state) => ({ ...state, isEnabled: !state.isEnabled })),
   on(CarActions.resetCar, (state) => initialState),
-  on(CarActions.setCar, (state, { car }) => ({
-    name: car.name,
-    price: car.price,
-    isEnabled: car.isEnabled,
-  }))
+  on(CarActions.setCar, (state, { car }) => ({ name: car.name, price: car.price, isEnabled: car.isEnabled })),
 );
 
 export function reducer(state: Car, action: Action) {
